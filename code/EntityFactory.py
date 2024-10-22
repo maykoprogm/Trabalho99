d26889fd54b074cdd67d71a302357ee083cf647a
Mayko <maykoftw6@hotmail.com>
2024-10-22 17:43:44 +0900

    EntityFactory, Background

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -19,12 +19,21 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
+
             case 'Level2Bg':
                 list_bg = []
                 for i in range(5):  # level2bg images number
                     list_bg.append(Background(f'Level2Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level2Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
+
+            case 'Level3Bg':
+                list_bg = []
+                for i in range(5):
+                    list_bg.append(Background(f'Level3Bg{i}', (0, 0)))
+                    list_bg.append(Background(f'Level3Bg{i}', (WIN_WIDTH, 0)))
+                return list_bg
+
             case 'Player1':
                 return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
             case 'Player2':
